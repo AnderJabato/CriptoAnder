38b8e89b63bb945e7552f07bce0f542807713d1c
AnderJabato <115215856+AnderJabato@users.noreply.github.com>
2023-11-14 00:42:51 +0100

    CoinMarket.js

--- components/CoinMarket.js
+++ components/CoinMarket.js
@@ -1,11 +1,58 @@
-import { StyleSheet, View, Text } from "react-native-web";
+import React from "react";
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from "react-native-web";
+const CoinMarket = ({ route }) => {
 
+  const { item } = route.params;
+  const coinItem = [item]
 
-const CoinMarket = ({ coin }) => {
-  console.log(coin)
   return (
     <View style={styles.containerItem}>
-      <Text style={styles.text}>CoinMarket</Text>
+      <View style={styles.header}>
+        <Text style={styles.title}>CryptoMarket</Text>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search a Coin"
+          placeholderTextColor="#858585"
+          onChangeText={(text) => text && setSearch(text)}
+        />
+      </View>
+
+      <View>
+        <Button
+          color="#121212"
+          style={styles.button}
+          title="Back"
+          onPress={() => navigation.navigate("Home")}
+        />
+      </View>
+
+      <View>
+        <FlatList
+          style={styles.list}
+          data={coinItem}
+          renderItem={({ item }) => (
+            <View>
+              {Object.keys(item).map((key) => (
+                <View
+                  key={key}
+                  style={styles.listText}
+                >
+                  <Text
+                    style={styles.text}
+                  >
+                    {key}
+                  </Text>
+                  <Text
+                    style={styles.text}
+                  >
+                    {item[key]}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          )}
+        />
+      </View>
     </View>
   )
 }
@@ -14,9 +61,22 @@ const styles = StyleSheet.create({
   containerItem: {
     backgroundColor: "#121212",
     paddingTop: 10,
-    flexDirection: "row",
+    flexDirection: "column",
     justifyContent: "space-between",
     cursor: "pointer"
+  },
+  header: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  searchInput: {
+    color: "#fff",
+    borderBottomColor: "#4657CE",
+    borderBottomWidth: 1,
+    width: "40%",
+    textAlign: "center",
   },
   containerNames: {
     marginLeft: 10,
@@ -48,6 +108,24 @@ const styles = StyleSheet.create({
     color: "#434343",
     textTransform: "uppercase",
   },
+  title: {
+    fontSize: 20,
+    color: "#fff",
+    marginTop: 10,
+  },
+  list: {
+    width: "100%",
+  },
+  listText: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: "100%",
+    alignItems: 'center',
+    border: '1px solid #e1e1e188',
+    padding: '15px'
+  },
 });
 
 export default CoinMarket
+
